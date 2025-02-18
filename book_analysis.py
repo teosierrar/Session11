352c8ccdee9da005d2d7fb11e05a1177964b2e34
teosierrar <teosierram@gmail.com>
2025-02-18 09:23:10 +0100

    second

--- book_analysis.py
+++ book_analysis.py
@@ -1,15 +1,24 @@
-book=requests.get("https://www.gutenberg.org/cache/epub/345/pg345.txt")
 
+import requests
 
-
-import
+book = requests.get('https://www.gutenberg.org/cache/epub/345/pg345.txt')
 print(book.text)
-lines=book.text.split('\n')
-punctuation_remove=",.!?;"
-punctuation_space="'\"()[]=-_"
-for lines in lines:
+lines = book.text.split('\n')
+punctuation_remove = ",.!?:;"
+punctuation_space = "',\()[]=-+_"
+unique_words = {}
+
+for line in lines:
+    # replace punctuation
     for c in punctuation_remove:
-        line=line.replace(c,"")
+        line = line.replace(c, " ")
     for c in punctuation_space:
-        line=line.replace(c," ")
-    print(line)
+        line = line.replace(c, " ")
+    words = line.split()
+    for word in words:
+        word = word.lower()
+        unique_words[word] = unique_words.get(word, 0) + 1
+
+print(unique_words)
+print(unique_words['the'])
+print(unique_words['dracula'])
--- dictionary_basics.py
+++ dictionary_basics.py
@@ -14,7 +14,7 @@ print(eng_to_spa)
 
 print("These are the spanish words thatI know")
 for key in eng_to_spa:
-    print(f"{eng_to_spa[key]} means {eng_to_spa[]}")
+    print(f"{eng_to_spa[key]} means {key}")
 
 print(dir(eng_to_spa))
 eng_spa2=eng_to_spa.copy()
@@ -23,7 +23,7 @@ print(eng_spa2)
 new_d=eng_spa2.fromkeys(eng_spa2,"YES")
 print(new_d)
 
-print(f"car in spanish is{eng_spa2('car','UNKNOWN')}")
+print(f"car in spanish is{eng_spa2.get('car',' UNKNOWN')}")
 print(list(eng_spa2.items()))
 print(eng_spa2)
 eng_spa2.setdefault("red","ROJO!!")
